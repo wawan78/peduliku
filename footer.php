10eb435dc3765e09df37a38992c02da88a4ce74a
Wawan <wawanirawan@daarululuumlido.com>
2025-03-08 16:48:10 +0700

    update 8mar2025 16:47

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <strong>&copy; 2025 <a href="https://daarululuumlido.com">Daarul Uluum Lido</a>.</strong>.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 1.0.0
+    </div>
+  </footer>
